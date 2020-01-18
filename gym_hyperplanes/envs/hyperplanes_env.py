219ebfe307fcb29f1967125f44f6b47f82db553b
genadi <genadi.rabinovich@moovit.com>
2020-01-18 18:55:29 +0200

    Initial commit

--- gym_hyperplanes/envs/hyperplanes_env.py
+++ gym_hyperplanes/envs/hyperplanes_env.py
@@ -1,11 +1,42 @@
 import gym
+import numpy as np
+from gym.spaces import Discrete, Tuple, Box
 
 
 class HyperPlanesEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
     def __init__(self):
-        pass
+        '''
+        If we have X features then for each hyper plane we have (X + 1) * 2 actions we van take
+        X - for angles on each dimension/features of hyperplane plus 1 for to/from origin
+        * 2 since it can be done in two directions (per action)
+        '''
+        # self.features = 18  # 3 actions per player, two player: 3 * 3 * 2
+        self.features = 2  # 3 actions per player, two player: 3 * 3 * 2
+        self.hyperplanes = 3
+        self.dimension = (self.features + 1) * 2 * self.hyperplanes
+        self.max_distance_from_origin = 100
+        self.distance_from_origin_delta_percents = 1
+
+        self.angle_delta = 45
+
+        self.action_space = Discrete(self.dimension)
+
+        l_states = []
+        for h in range(0, self.hyperplanes):
+            for f in range(self.features):
+                l_states.append(Discrete(180 - self.angle_delta))
+            l_states.append(Discrete(100 / self.distance_from_origin_delta_percents))
+        self.observation_space = Tuple(l_states)
+
+        # lows = [0] * (self.features + 1) * self.hyperplanes
+        # highs = []
+        # for h in range(0, self.hyperplanes):
+        #     for f in range(self.features):
+        #         highs.append(180 - self.angle_delta)
+        #     highs.append(100 / self.distance_from_origin_delta_percents)
+        # self.observation_space = Box(lows, highs, dtype=np.float32, shape=(1, (self.features + 1) * self.hyperplanes))
 
     def step(self, action):
         pass
@@ -17,4 +48,7 @@ class HyperPlanesEnv(gym.Env):
         pass
 
     def close(self):
+        pass
+
+    def configure(self, key, value):
         pass
