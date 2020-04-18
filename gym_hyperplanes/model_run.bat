6bb59b0d88d970c2ee2dc4c493377ed2af636bf6
genadi <genadi.rabinovich@moovit.com>
2020-04-18 18:04:03 +0000

    more logs and small formatting

--- gym_hyperplanes/model_run.bat
+++ gym_hyperplanes/model_run.bat
@@ -1,3 +1,4 @@
+echo off
 python model_run.py -c /UP/Teza/classoptimizer/gym-hyperplanes/gym_hyperplanes/data/Games/config_model.ini
 rem python model_run.py -c /UP/Teza/classoptimizer/gym-hyperplanes/gym_hyperplanes/data/iris/config_model.ini
 rem python model_run.py -c /UP/Teza/classoptimizer/gym-hyperplanes/gym_hyperplanes/data/pendigits/config_model.ini
--- gym_hyperplanes/optimizer_run.bat
+++ gym_hyperplanes/optimizer_run.bat
@@ -1,3 +1,4 @@
+echo off
 python optimizer_run.py -c /UP/Teza/classoptimizer/gym-hyperplanes/gym_hyperplanes/data/Games/config_solver.ini
 rem python optimizer_run.py -c /UP/Teza/classoptimizer/gym-hyperplanes/gym_hyperplanes/data/iris/config_solver.ini
 rem python optimizer_run.py -c /UP/Teza/classoptimizer/gym-hyperplanes/gym_hyperplanes/data/pendigits/config_solver.ini
